d52b15fa19844180f15283f26555f169d28d4d4e
Seva Safris <seva@safris.org>
2018-09-13 23:02:06 +0700

    Move libx4j to openjax

--- src/test/java/org/fastjax/cdm/lexer/LexerTest.java
+++ src/test/java/org/fastjax/cdm/lexer/LexerTest.java
@@ -28,7 +28,7 @@ import org.junit.Test;
 public class LexerTest {
   @Test
   public void testListener() throws IOException {
-    final String source = "/* Test class */\r// With a comment\npackage org.libx4j.cdm.lexer;\npublic class StringTest implements com.logicbig.example.ITest{" + "public void doSomething(){" + "System.out.println(\"testing\");}}";
+    final String source = "/* Test class */\r// With a comment\npackage " + getClass().getPackageName() + ";\npublic class StringTest implements com.logicbig.example.ITest{" + "public void doSomething(){" + "System.out.println(\"testing\");}}";
     final Audit audit = Lexer.tokenize(new StringReader(source), source.length(), new Lexer.Token.Listener() {
       private boolean packageFound = false;
       private int start = -1;
@@ -41,7 +41,7 @@ public class LexerTest {
           if (this.start == -1 && token == Lexer.Span.WORD)
             this.start = start;
           else if (token == Lexer.Delimiter.SEMI_COLON) {
-            Assert.assertEquals("org.libx4j.cdm.lexer", source.substring(this.start, start));
+            Assert.assertEquals(getClass().getPackageName(), source.substring(this.start, start));
             return false;
           }
         }
@@ -50,12 +50,13 @@ public class LexerTest {
       }
     });
 
-    Assert.assertEquals("/* Test class */\r// With a comment\npackage org.libx4j.cdm.lexer;", audit.toString());
+    Assert.assertEquals("/* Test class */\r// With a comment\npackage " + getClass().getPackageName() + ";", audit.toString());
   }
 
   @Test
+  // FIXME: This is a bad test!
   public void testTokenize() throws IOException {
-    final File file = new File("../../libx4j/xsb/runtime/src/main/java/org/libx4j/xsb/runtime/Binding.java");
+    final File file = new File("../../openjax/xsb/runtime/src/main/java/org/openjax/xsb/runtime/Binding.java");
     final Audit audit = Lexer.tokenize(file, null);
 
     final String expected = new String(Files.readAllBytes(file.toPath()));
