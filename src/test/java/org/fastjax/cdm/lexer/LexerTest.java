13bbd892e54392621b985b0d5d3209c5c03b2012
Seva Safris <seva@safris.org>
2018-12-08 19:47:53 +0700

    Conform to jdk1.8

--- src/test/java/org/fastjax/cdm/lexer/LexerTest.java
+++ src/test/java/org/fastjax/cdm/lexer/LexerTest.java
@@ -29,7 +29,7 @@ import org.junit.Test;
 public class LexerTest {
   @Test
   public void testListener() throws IOException {
-    final String source = "/* Test class */\r// With a comment\npackage " + getClass().getPackageName() + ";\npublic class StringTest implements com.logicbig.example.ITest{" + "public void doSomething(){" + "System.out.println(\"testing\");}}";
+    final String source = "/* Test class */\r// With a comment\npackage " + getClass().getPackage().getName() + ";\npublic class StringTest implements com.logicbig.example.ITest{" + "public void doSomething(){" + "System.out.println(\"testing\");}}";
     final Audit audit = Lexer.tokenize(new StringReader(source), source.length(), new Lexer.Token.Listener() {
       private boolean packageFound = false;
       private int start = -1;
@@ -42,7 +42,7 @@ public class LexerTest {
           if (this.start == -1 && token == Lexer.Span.WORD)
             this.start = start;
           else if (token == Lexer.Delimiter.SEMI_COLON) {
-            assertEquals(getClass().getPackageName(), source.substring(this.start, start));
+            assertEquals(getClass().getPackage().getName(), source.substring(this.start, start));
             return false;
           }
         }
@@ -51,7 +51,7 @@ public class LexerTest {
       }
     });
 
-    assertEquals("/* Test class */\r// With a comment\npackage " + getClass().getPackageName() + ";", audit.toString());
+    assertEquals("/* Test class */\r// With a comment\npackage " + getClass().getPackage().getName() + ";", audit.toString());
   }
 
   @Test
